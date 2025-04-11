702452289cc38313f518af023c621f1f0025429a
jnthnluong <jnthnluong@gmail.com>
2025-04-11 11:02:13 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -14,7 +14,7 @@ public class OnlinePurchaseManager
     /* to be implemented in part (a) */
     int count = 0;
     for(int i = 0; i < purchases.size(); i++){
-      if(purchases.get(i).isElectronic() && purchases.get(i).getMaker().equals(maker)
+      if(purchases.get(i).isElectronic() && purchases.get(i).getMaker().equals(maker))
          count++;
       
     }
